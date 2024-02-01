8adaa56532b91cd7c4262e9fe2084d0b6484367d
Emilian Klein <emilian.klein@protonmail.com>
2024-02-01 17:54:45 +0100

    Code cleanup.

--- dice_roller.py
+++ dice_roller.py
@@ -21,21 +21,29 @@ class DiceRoller(tk.Tk):
         self.iconbitmap("images/icon.ico")
         self.sm_font_style = ("Helvetica", "10", "bold")
         self.lg_font_style = ("Helvetica", "13", "bold")
+
         self.top_frame = tk.Frame(self)
         self.top_frame.pack(pady=10)
+
         self.dice_image1 = tk.Label(self.top_frame)
         self.dice_image1.pack(side="left")
+
         self.dice_image2 = tk.Label(self.top_frame)
         self.dice_image2.pack(side="left")
+
         self.middle_frame = tk.Frame(self)
         self.middle_frame.pack()
+
         self.total_value_label = tk.Label(self.middle_frame, fg="cornflowerblue", font=self.lg_font_style)
         self.total_value_label.pack()
+
         self.bottom_frame = tk.Frame(self)
         self.bottom_frame.pack(pady=10)
+
         self.roll_button = tk.Button(self.bottom_frame, text="Roll dices", fg="white", bg="cornflowerblue", relief="groove", command=self.roll_dices,
                                      font=self.sm_font_style, width=10, cursor="hand2")
         self.roll_button.pack(side="left", padx=10)
+
         self.exit_button = tk.Button(self.bottom_frame, text="Exit", fg="white", bg="cornsilk4", relief="groove", command=self.quit, font=self.sm_font_style,
                                      width=10, cursor="hand2")
         self.exit_button.pack(side="left")
